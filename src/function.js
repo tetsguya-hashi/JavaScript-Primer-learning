4213c1fa6506b85d88fafefb28b4f6676b7e3579
tetsuya-hashi <99647449+tetsguya-hashi@users.noreply.github.com>
2022-06-05 13:17:35 +0900

    function this start

--- src/function.js
+++ src/function.js
@@ -15,3 +15,33 @@
 }
 console.log(str2); //bbb
 // console.log(str);/エラー
+{
+  console.log("---this---");
+  const obj = {
+    method1: function () {
+      return this;
+    },
+    method2: function () {
+      return this;
+    }
+  };
+  console.log(obj.method1()); //objの中身が帰って来る{method1: ƒ method1(), method2: ƒ method2()}
+  const person = {
+    fullName: "Brendan Eich",
+    sayName: function () {
+      return this.fullName;
+    }
+  };
+  console.log(person.sayName());
+  const obj1 = {
+    obj2: {
+      obj3: {
+        method() {
+          return this;
+        }
+      }
+    }
+  };
+  //thisは自分の親を指す、祖オブジェクトではない
+  console.log(obj1.obj2.obj3.method() === obj1.obj2.obj3); //true
+}
