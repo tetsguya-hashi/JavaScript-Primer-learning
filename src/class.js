850fe4ee1445a0515009102b59c07b056688dcb1
tetsuya-hashi <99647449+tetsguya-hashi@users.noreply.github.com>
2022-06-11 07:18:41 +0900

    class finish

--- src/class.js
+++ src/class.js
@@ -81,13 +81,90 @@
   const increment = counter.increment;
 }
 {
-  console.log("---静的メゾット---");
-  //クラスをインスタンス化せずに利用する
-}
-{
   console.log("プロトタイプオブジェクト");
   function fn() {}
   console.log(typeof fn.prototype === "object"); //true
   class MyClass {}
   console.log(typeof MyClass.prototype === "object"); //true
+}
+{
+  console.log("---super---");
+  class Parent {
+    constructor(...args) {
+      console.log("Parentコンストラクタの処理", ...args);
+    }
+  }
+  //Parentを継承したクラスの定義
+  class Child extends Parent {
+    constructor(...args) {
+      //Parentのコンストラクタ処理を呼び出す
+      super(...args);
+      console.log("Childコンストラクタの処理", ...args);
+    }
+  }
+  const child = new Child("引数１", "引数２");
+
+  //コンストラクタの処理順は親クラスから小クラスへ
+  class Parent1 {
+    constructor() {
+      this.name = "Parent";
+    }
+  }
+  class Child1 extends Parent1 {
+    constructor() {
+      //thisの前に親要素を呼びだす
+      super();
+      //小クラスのコンストラクタ処理（上書き）
+      this.name = "Child";
+    }
+  }
+  const parent1 = new Parent1();
+  console.log(parent1.name); //parent
+  const child1 = new Child1();
+  console.log(child1.name); //Child
+
+  //superプロパティ
+  console.log("---super---");
+  class Parent2 {
+    method() {
+      console.log("Parent.prototype.method");
+    }
+  }
+  class Child2 extends Parent2 {
+    method() {
+      console.log("Child.portotype.method");
+      super.method();
+    }
+  }
+  const child2 = new Child2();
+  child2.method();
+}
+{
+  //継承の判定
+  class Parent {}
+  class Child extends Parent {}
+
+  const parent = new Parent();
+  const child = new Child();
+  //ParentのインスタンスはParentのみを継承したインスタンス
+  console.log(parent instanceof Parent); //true
+  console.log(parent instanceof Child); //false
+  //ChildのインスタンスはChildとParentを継承したインスタンス
+  console.log(child instanceof Parent); //true
+  console.log(child instanceof Child); //true
+}
+{
+  //ビルトインオブジェクトの継承
+  class MyArray extends Array {
+    get first() {
+      return this.at(0);
+    }
+    get last() {
+      return this.at(-1);
+    }
+  }
+  const array = MyArray.from([1, 2, 3, 4, 5, 6]);
+  console.log(array.length); //6
+  console.log(array.first); //1
+  console.log(array.last); //6
 }
