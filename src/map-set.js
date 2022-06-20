9b32698cb21087360c98bb5ebe59857a25cb0577
tetsuya-hashi <99647449+tetsguya-hashi@users.noreply.github.com>
2022-06-21 07:09:47 +0900

    map set 2day finish

--- src/map-set.js
+++ src/map-set.js
@@ -22,3 +22,35 @@ const マップの反復処理 = (() => {
   });
   console.log(results); //["key1:value1", "key2:value2"]
 })();
+const セットの作成と初期化 = (() => {
+  const set = new Set();
+  console.log(set.size); //0
+})();
+
+const set値の追加と取り出し = (() => {
+  const set = new Set();
+  set.add("a");
+  console.log(set.size);
+  //同じ値は追加されない
+  set.add("a");
+  console.log(set.size);
+  //値の確認
+  console.log(set.has("a")); //true
+  console.log(set.has("b")); //false
+})();
+const setの反復処理 = (() => {
+  const set = new Set(["a", "b"]);
+  const results = [];
+  set.forEach((value) => {
+    results.push(value);
+  });
+  console.log(results); //['a','b']
+})();
+const setのforOf = () => {
+  const set = new Set(["a", "b"]);
+  const results = [];
+  for (const value of set) {
+    results.push(value);
+  }
+  console.log(results);
+};
